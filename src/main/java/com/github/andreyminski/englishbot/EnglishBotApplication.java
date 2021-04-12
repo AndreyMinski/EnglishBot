e12fe3c44894d189c094f1f6336e67e75f97ddaf
madhead <siarhei.krukau@gmail.com>
2021-04-13 00:54:00 +0200

    Fix #1: Application skeleton

---
+++ src/main/java/com/github/andreyminski/englishbot/EnglishBotApplication.java
@@ -0,0 +1,11 @@
+package com.github.andreyminski.englishbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnglishBotApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EnglishBotApplication.class, args);
+    }
+}
